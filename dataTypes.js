c89863f47b60ec7265a05b42c33408e727f982ad
srinivassri2411 <srinivashayagriva@gmail.com>
2026-04-17 08:06:11 +0530

    learnt about datatypes, decleation, loops, incrementanddecrement, arrays basic

---
+++ dataTypes.js
@@ -0,0 +1,18 @@
+var a=10;
+let b=20;
+const c=30;
+console.log(a);
+console.log(b);
+console.log(c);
+
+a=20;
+console.log(a);
+b=30;
+console.log(b)
+// c=40;
+
+
+console.log(typeof("String"));
+console.log(typeof(10));
+console.log(typeof(false));
+console.log(typeof(null));
